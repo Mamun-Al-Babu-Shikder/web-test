5019e99194164aea3dd298d15cfc1eb6502194a1
mamun <abdullah.mamun@dsinnovators.com>
2023-10-06 12:31:46 +0600

    Initial commit

---
+++ src/main/java/com/mcubes/webtest/WebTestApplication.java
@@ -0,0 +1,13 @@
+package com.mcubes.webtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcubes/webtest/WebTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcubes.webtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
