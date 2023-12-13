bc4d2337e4ef68a605886ba8fb9aa2da96c0eedd
Mamun-Al-Babu-Shikder <aalmamun295@gmail.com>
2023-12-13 17:48:57 +0600

    add some web element operation and WebElementOperationException class

--- src/main/java/com/mcubes/webtest/actions/web/WebElementOperation.java
+++ src/main/java/com/mcubes/webtest/actions/web/WebElementOperation.java
@@ -4,16 +4,24 @@ import com.mcubes.webtest.actions.Action;
 import com.mcubes.webtest.core.ExpEvaluator;
 import com.mcubes.webtest.core.StepContext;
 import com.mcubes.webtest.core.WebElementProvider;
+import com.mcubes.webtest.exception.AttributeNotFoundException;
+import com.mcubes.webtest.exception.InvalidAttributeValueException;
 import com.mcubes.webtest.exception.InvalidTypeException;
+import com.mcubes.webtest.exception.WebElementOperationException;
 import com.mcubes.webtest.util.Utils;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
 
+import static com.mcubes.webtest.actions.web.WebElementOperation.OperationType.ENTER_TEXT;
+import static com.mcubes.webtest.actions.web.WebElementOperation.OperationType.GET_ATTR;
 import static com.mcubes.webtest.constants.JsonAttributeKeys.*;
+import static com.mcubes.webtest.enums.ActionType.WEB_ELEMENT_OPT;
 
 public class WebElementOperation implements Action {
     private String varName;
@@ -33,21 +41,30 @@ public class WebElementOperation implements Action {
         String operationType = object.getString(OPERATION);
         this.operationType = OperationType.from(operationType);
 
-        String varName = object.optString(VAR);
+        String varName = object.optString(VAR, null);
         if (varName != null)
             this.varName = Utils.validateAndGetVarName(varName);
 
-        JSONArray params = object.optJSONArray(PARAMS);
-        if (params != null) {
-            for (int i=0; i<params.length(); i++) {
-                this.params.add(params.get(i));
+        Object paramsObject = object.opt(PARAMS);
+
+        if (paramsObject == null && (this.operationType == ENTER_TEXT || this.operationType == GET_ATTR)) {
+            throw new AttributeNotFoundException("Required attribute 'params' not found for '%s' -> '%s'"
+                    .formatted(WEB_ELEMENT_OPT.value(), this.operationType.value()));
+        }
+
+        if (paramsObject != null) {
+            if (paramsObject instanceof JSONArray paramsArray) {
+                for (int i = 0; i < paramsArray.length(); i++) {
+                    this.params.add(paramsArray.get(i));
+                }
+            } else {
+                this.params.add(paramsObject);
             }
         }
 
-        if (this.operationType == OperationType.ENTER_TEXT && this.params.isEmpty()) {
-            throw new IllegalArgumentException();
-        } else if (this.operationType == OperationType.GET_ATTR && this.params.isEmpty()) {
-            throw new IllegalArgumentException();
+        if ((this.operationType == ENTER_TEXT || this.operationType == GET_ATTR) && this.params.isEmpty()) {
+            throw new InvalidAttributeValueException("The '%s' -> '%s' requires at least 1 'params'"
+                    .formatted(WEB_ELEMENT_OPT.value(), this.operationType.value()));
         }
     }
 
@@ -56,36 +73,52 @@ public class WebElementOperation implements Action {
         WebElement element = webElementProvider != null ? webElementProvider.getWebElement(stepContext)
                 : ExpEvaluator.evaluate(stepContext, elementObject, WebElement.class);
 
-
-        Object returnValue = switch (operationType) {
-            case CLICK -> {
-                element.click();
-                yield null;
-            }
-            case ENTER_TEXT -> {
-                StringJoiner sj = new StringJoiner(" ");
-                params.forEach(p -> sj.add(p.toString()));
-                element.sendKeys(sj.toString());
-                yield null;
-            }
-            case IS_DISPLAYED -> element.isDisplayed();
-            case IS_SELECTED -> element.isSelected();
-            case GET_ATTR -> element.getAttribute(params.get(0).toString());
-            default -> null;
-        };
+        Object returnValue;
+        try {
+            returnValue = switch (operationType) {
+                case CLICK -> {
+                    element.click();
+                    yield null;
+                }
+                case ENTER_TEXT -> {
+                    StringJoiner sj = new StringJoiner(" ");
+                    params.forEach(p -> sj.add(p.toString()));
+                    element.sendKeys(sj.toString());
+                    yield null;
+                }
+                case IS_DISPLAYED -> element.isDisplayed();
+                case IS_SELECTED -> element.isSelected();
+                case GET_ATTR -> element.getAttribute(params.get(0).toString());
+                case GET_TEXT -> element.getText();
+                case GET_TAG_NAME -> element.getTagName();
+                case DOM_ATTR -> element.getDomAttribute(params.get(0).toString());
+                case DOM_PROP -> element.getDomProperty(params.get(0).toString());
+                case CLEAR -> {
+                    element.clear();
+                    yield null;
+                }
+            };
+        } catch (Exception ex) {
+            throw new WebElementOperationException("Error occurred while execute '%s' -> '%s'"
+                    .formatted(WEB_ELEMENT_OPT.value(), operationType.value()));
+        }
 
         if (varName != null) {
             stepContext.set(varName, returnValue);
         }
     }
 
-    private enum OperationType {
+    public enum OperationType {
         CLICK("click"),
         ENTER_TEXT("enter_text"),
         IS_DISPLAYED("is_displayed"),
         IS_SELECTED("is_selected"),
-        GET_ATTR("get_attr")
-        ;
+        GET_ATTR("get_attr"),
+        GET_TEXT("get_text"),
+        GET_TAG_NAME("get_tag"),
+        DOM_ATTR("dom_attr"),
+        DOM_PROP("dom_prop"),
+        CLEAR("clear");
 
         private final String value;
 
@@ -93,12 +126,17 @@ public class WebElementOperation implements Action {
             this.value = value;
         }
 
+        public String value() {
+            return value;
+        }
+
         public static OperationType from(String type) {
             final String trimmedType = type.trim();
             return Arrays.stream(OperationType.values())
                     .filter(p -> p.value.equals(trimmedType))
                     .findFirst()
-                    .orElseThrow(() -> new InvalidTypeException("Failed to resolve web element operation type ['%s']".formatted(trimmedType)));
+                    .orElseThrow(() -> new InvalidTypeException("Failed to resolve web element operation type ['%s']"
+                            .formatted(trimmedType)));
         }
     }
 }
---
+++ src/main/java/com/mcubes/webtest/exception/WebElementOperationException.java
@@ -0,0 +1,7 @@
+package com.mcubes.webtest.exception;
+
+public class WebElementOperationException extends RuntimeException {
+    public WebElementOperationException(String message) {
+        super(message);
+    }
+}
